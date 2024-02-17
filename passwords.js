3a82c3763f4f460a103064fad3856c984011e5f6
MKBAL Idris <mekbalidriss@gmail.com>
2024-02-17 15:46:20 +0100

    some updates in the input of

--- passwords.js
+++ passwords.js
@@ -1,22 +1,30 @@
 const generate=document.getElementById("generate");
-const number=document.getElementById("generate");
-const special=document.getElementById("generate");
-const capital=document.getElementById("generate");
+const number=document.getElementById("number");
+const special=document.getElementById("special");
+const capital=document.getElementById("capital");
 const pass=document.getElementById("pass");
 const checknum=document.getElementById("checknum");
 const checkspecial=document.getElementById("checkspecial");
 const checkcapital=document.getElementById("checkcapital");
+const txt=document.getElementById("txt");
 
 
 generate.onclick=function(){
-    if(checknum.checked && checkspecial.checked && checkcapital.checked){
+    let pwlength = 0;
+    const numbervalue = Number(number.value);
+    const specialvalue = Number(special.value);
+    const capitalvalue = Number(capital.value);
     
+    if(checknum.checked && checkspecial.checked && checkcapital.checked){
+        pwlength = numbervalue + specialvalue + capitalvalue;
+        txt.textContent = pwlength;
     }
     if(checkspecial.checked){
-    
+
     }
     if(checkcapital.checked){
-    
+
     }
+
 }
 
